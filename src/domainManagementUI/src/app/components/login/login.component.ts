4bf168be09295fc2c45fbb2985fee3a7e8b96f1e
Dylan Johnson <dylanj1752@gmail.com>
2021-02-10 21:51:04 -0700

    login base

---
+++ src/domainManagementUI/src/app/components/login/login.component.ts
@@ -0,0 +1,28 @@
+//Angular Imports
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  test() {}
+}
