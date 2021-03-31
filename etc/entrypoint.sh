3d2bb327037ea1596f57912fbedba1c8e4ec0fc7
Zachary Wilson <zachary.wilson@inl.gov>
2021-03-31 10:35:33 -0600

    update container tests

--- etc/entrypoint.sh
+++ etc/entrypoint.sh
@@ -4,4 +4,5 @@ cd /app || exit
 echo "Substituting settings"
 envsubst < /usr/share/nginx/html/assets/settings.template.json > /usr/share/nginx/html/assets/settings.json
 
+echo "Starting nginx"
 exec nginx -g 'daemon off;'
