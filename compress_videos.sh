ff555a9b7266e0a69abc4cd0b6b740ff388e256d
Zachary Wilson <zachary.wilson@inl.gov>
2021-05-06 10:19:35 -0600

    add tutorial videos

---
+++ compress_videos.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+BASE_PATH="src/domainManagementUI/src/assets/videos"
+
+for f in $BASE_PATH/*.mp4;
+do
+    ffmpeg -i "$f" "$BASE_PATH/output.mp4" -y
+    cp "$BASE_PATH/output.mp4" $f
+done
+
+rm "$BASE_PATH/output.mp4"
--- src/domainManagementUI/src/app/components/help/video-tutorials/video-tutorials.component.ts
+++ src/domainManagementUI/src/app/components/help/video-tutorials/video-tutorials.component.ts
@@ -6,7 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-tutorials.component.scss'],
 })
 export class VideoTutorialsComponent implements OnInit {
+  applicationPlaylist = [
+    {
+      name: 'Add an Application',
+      src: 'assets/videos/application_add.mp4',
+    },
+    {
+      name: 'Delete an Application',
+      src: 'assets/videos/application_delete.mp4',
+    },
+  ];
+
+  domainPlaylist = [];
+
+  templatePlaylist = [];
+
+  userPlaylist = [
+    {
+      name: 'Register User',
+      src: 'assets/videos/user_register.mp4',
+    },
+    {
+      name: 'Confirm User',
+      src: 'assets/videos/user_confirm.mp4',
+    },
+    {
+      name: 'Disable/Delete User',
+      src: 'assets/videos/user_disable_delete.mp4',
+    },
+    {
+      name: 'Set User Applications',
+      src: 'assets/videos/user_applications.mp4',
+    },
+    {
+      name: 'Generate API Key',
+      src: 'assets/videos/user_apikey.mp4',
+    },
+  ];
+
+  currentVid = {
+    src: '',
+  };
+
+  player: any;
+
   constructor() {}
+
+  playVideo() {
+    this.player.play();
+  }
+
+  selectVideo(video: any) {
+    this.currentVid = video;
+  }
+
+  onPlayerReady(player: any) {
+    this.player = player;
+    this.player
+      .getDefaultMedia()
+      .subscriptions.loadedMetadata.subscribe(this.playVideo.bind(this));
+  }
 
   ngOnInit(): void {}
 }
