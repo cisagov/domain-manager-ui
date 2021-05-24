aeba37cb5259ff336036fbfe6c97f81e7b1f8263
Zachary Wilson <zachary.wilson@inl.gov>
2021-05-24 10:21:26 -0600

    Rename hosted zones to hosted zone

--- src/domainManagementUI/src/app/components/domain/domain-details/tabs/hosted-zone/domain-details-hosted-zone.component.ts
+++ src/domainManagementUI/src/app/components/domain/domain-details/tabs/hosted-zone/domain-details-hosted-zone.component.ts
@@ -1,20 +1,15 @@
 // Angular Imports
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 // Local Service Imports
-import { DomainService } from 'src/app/services/domain.service';
 import { DomainDetailsTabService } from 'src/app/services/tab-services/domain-details-tabs.service';
 
-//Models
-import { DomainModel } from 'src/app/models/domain.model';
-
 @Component({
-  selector: 'dd-hosted-zones',
-  templateUrl: './domain-details-hosted-zones.component.html',
-  styleUrls: ['./domain-details-hosted-zones.component.scss'],
+  selector: 'dd-hosted-zone',
+  templateUrl: './domain-details-hosted-zone.component.html',
+  styleUrls: ['./domain-details-hosted-zone.component.scss'],
 })
-export class DomainDetailsHostedZonesComponent implements OnInit, OnDestroy {
+export class DomainDetailsHostedZoneComponent implements OnInit, OnDestroy {
   component_subscriptions = [];
 
   constructor(public ddTabSvc: DomainDetailsTabService) {}
