c36745609029c895d5ab60b91cd0d099d8d3bd96
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-09-01 22:35:21 -0500

    fix missing categorize dropdown

--- src/domainManagementUI/src/app/components/categorization/tabs/categorization-submit/categorization-submit.component.ts
+++ src/domainManagementUI/src/app/components/categorization/tabs/categorization-submit/categorization-submit.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+// Angular Imports
+import { Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+// Local Service Imports
 import { LayoutService } from 'src/app/services/layout.service';
 import { CategorizationTabService } from 'src/app/services/tab-services/categorization-tabs.service';
+
 @Component({
   selector: 'app-categorization-submit',
   templateUrl: './categorization-submit.component.html',
   styleUrls: ['./categorization-submit.component.scss'],
 })
 export class CategorizationSubmitComponent {
+  categoryData = [];
+  displayedColumns = [
+    'domain',
+    'proxy',
+    'status',
+    'category',
+    'created',
+    'categorize',
+  ];
+  categoryList: MatTableDataSource<any> = new MatTableDataSource<any>();
+
+  @ViewChild(MatSort) sort: MatSort;
   constructor(
     public layoutSvc: LayoutService,
     public categorizationTabSvc: CategorizationTabService
