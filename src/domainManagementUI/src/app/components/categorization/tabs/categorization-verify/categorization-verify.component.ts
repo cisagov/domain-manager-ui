cd4a46e02858ba54fa9464b41babc5e7f398fd88
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-09-01 22:48:05 -0500

    update mat table for categorization page

--- src/domainManagementUI/src/app/components/categorization/tabs/categorization-verify/categorization-verify.component.ts
+++ src/domainManagementUI/src/app/components/categorization/tabs/categorization-verify/categorization-verify.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+// Angular Imports
+import { Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+// Local Service Imports
 import { LayoutService } from 'src/app/services/layout.service';
 import { CategorizationTabService } from 'src/app/services/tab-services/categorization-tabs.service';
 @Component({
@@ -7,6 +12,18 @@ import { CategorizationTabService } from 'src/app/services/tab-services/categori
   styleUrls: ['./categorization-verify.component.scss'],
 })
 export class CategorizationVerifyComponent {
+  categoryData = [];
+  displayedColumns = [
+    'domain',
+    'proxy',
+    'status',
+    'category',
+    'created',
+    'verify',
+  ];
+  categoryList: MatTableDataSource<any> = new MatTableDataSource<any>();
+
+  @ViewChild(MatSort) sort: MatSort;
   constructor(
     public layoutSvc: LayoutService,
     public categorizationTabSvc: CategorizationTabService
